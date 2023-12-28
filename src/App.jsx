cba81240c1ff6b4f85fd6bf1d084db0a2923897d
Rudra006 <rudramadhab006@gmail.com>
2023-12-28 17:56:29 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Calendar from "./pages/Calendar/Calendar";
+import Layout from "./components/Layout/Layout";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BoardPage from "./pages/Board/Board";
+import DataGrid from "./pages/DataGrid/DataGrid";
+
+const App = () => {
+  return (
+    <div id="dashboard">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="calendar" element={<Calendar />} />
+            <Route path="board" element={<BoardPage />} />
+            <Route path="users" element={<DataGrid />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
